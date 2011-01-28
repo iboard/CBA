3d5ba774e17e87b910437f36f0470331fe9c3b00
Andi Altendorfer <andi@iboard.cc>
2011-01-28 22:38:43 +0100

    Google Analytics Code - fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,6 @@ class ApplicationController < ActionController::Base
   # Load all pages marked with 'show_in_menu => true'
   before_filter  :load_top_pages
   
-  # set Google Analytics Script for header
-  before_filter  :set_google_analytics
-
   # == Display a flash if CanCan doesn't allow access    
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = exception.message
@@ -22,20 +19,9 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
   def load_top_pages
     @top_pages ||= Page.where(:show_in_menu => true).asc(:menu_order)
   end
-  
-  # yield :google_analytics will be loaded in HTML-HEAD
-  def set_google_analytics
-    if (Rails.env == 'production') && 
-       File::exists?(
-         filename=File.join(Rails.root,"config/google_analytics.header")
-       )
-      content_for :google_analytics do
-        File.new(filename).read.to_s
-      end
-    end
-  end
-  
+   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,15 @@
 #
 module ApplicationHelper
   
+  # yield :google_analytics will be loaded in HTML-HEAD
+  def insert_google_analytics_script
+    if File::exist?(
+         filename=File::join(Rails.root,"config/google_analytics.head")
+       )
+       File.new(filename).read.html_safe
+    end
+  end
+  
   # Return the field if current_user or the default if not
   def current_user_field(fieldname,default='')
     if user_signed_in?

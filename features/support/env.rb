934ef7c1d07c3ffdd6ed338d7fa367e59c977960
Andi Altendorfer <andi@iboard.cc>
2011-05-08 17:56:47 +0200

    Blog index review

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require 'simplecov'
 SimpleCov.start do
   add_filter "/test/"
+  add_filter "/features/"
 end
 
 require 'rubygems'
@@ -13,7 +14,6 @@ Spork.prefork do
   require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
   require 'cucumber/rails/rspec'
   require 'cucumber/rails/world'
-  #require 'cucumber/rails/active_record'
   require 'cucumber/web/tableish'
 
   require 'capybara/rails'
@@ -21,18 +21,18 @@ Spork.prefork do
   require 'capybara/session'
 
   Dir.new("#{Rails.root}/test/factories/").reject {|r| !r.match(/\.rb$/) }.each do |factory|
-    require "#{Rails.root}/test/factories/#{factory}"  
+    require "#{Rails.root}/test/factories/#{factory}"
   end
 
   require 'factory_girl'
-  require 'factory_girl/step_definitions'  
-  
+  require 'factory_girl/step_definitions'
+
   # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
   # order to ease the transition to Capybara we set the default here. If you'd
   # prefer to use XPath just remove this line and adjust any selectors in your
   # steps to use the XPath syntax.
   Capybara.default_selector = :css
-  
+
 end
 
 Spork.each_run do
@@ -41,5 +41,5 @@ Spork.each_run do
     'spec','factories','*.rb'))].each { |f| require f }
 end
 
-  
+
 

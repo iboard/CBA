4cd3fd5273c3d69a5c6027672e0a7be991976c7b
Andi Altendorfer <andi@iboard.cc>
2011-06-02 11:37:10 +0200

    Adding twitter.env.html box

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper # :nodoc:
+  
+  # Render a twitter-box of config/twitter.html exists
+  def insert_twitter_box
+    if File.exist?( filename=File.expand_path("../../../config/twitter.#{Rails.env}.html", __FILE__))
+       File.new(filename).read.html_safe
+    end
+  end
+  
+  
 end

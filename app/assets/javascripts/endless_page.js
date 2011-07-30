1c9d90d1b384816e79788bac3fd10aaec5228e1e
Andi Altendorfer <andi@iboard.cc>
2011-07-30 12:31:02 +0200

    Added ScrollToTop-button

--- app/assets/javascripts/endless_page.js
+++ app/assets/javascripts/endless_page.js
@@ -1,12 +1,28 @@
 var currentPage = 1;
+var state_scroll_to_top_link = 0;
 
-function checkScroll() {
+function checkScroll() {  
   if (nearBottomOfPage()) {
     currentPage++;
     $('#load_more_link').click();
   } else {
     setTimeout("checkScroll()", 500);
+  }  
+}
+
+function controllToTopLink() {
+  if( onTop() ) {
+    if( state_scroll_to_top_link == 1 ) {
+      $('#scroll-to-top').fadeTo(250,0.0);
+      state_scroll_to_top_link = 0;
+    }
+  } else {
+    if( state_scroll_to_top_link == 0 ) {
+      $('#scroll-to-top').fadeTo(250,0.75);
+      state_scroll_to_top_link = 1;
+    }
   }
+  setTimeout("controllToTopLink()", 500);
 }
 
 function nearBottomOfPage() {
@@ -21,6 +37,16 @@ function pageHeight() {
   return Math.max(document.body.scrollHeight, document.body.offsetHeight);
 }
 
+function onTop() {
+  var where = ((document.body.scrollHeight)-scrollDistanceFromBottom())/document.body.scrollHeight*100;
+  var visible=0;
+  var f = where-visible;
+  $("#scroll-position").html(sprintf("%d%",f));
+  return(window.pageYOffset <= 0);
+}
+
 $(document).ready( function() {
+  $('#scroll-to-top').hide();
+  controllToTopLink();
   checkScroll();
 });

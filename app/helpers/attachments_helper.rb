3a3eee575e27c92cb096ff3760634d670a8eb484
Andi Altendorfer <andi@iboard.cc>
2011-08-26 12:28:06 +0200

    Review form-styles for attachments

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,13 +1,22 @@
 module AttachmentsHelper # :nodoc:
 
   def label_for_attachment(f)
-    rc =  "<b>"
-    rc +=   link_to( f.object.file_file_name, f.object.file.url(:original) )
-    rc +=   " "
-    rc +=   (f.object.file_file_size/1024).round.to_s
-    rc +=   " KB, "+f.object.file_content_type
-    rc += "</b>"
-    rc
+    rc  = link_to( link_text(f.object), f.object.file.url(:original), :class => 'nostyle-link' )
+    rc += " "
+    rc += (f.object.file_file_size/1024).round.to_s
+    rc += " KB, "+f.object.file_content_type
+    raw rc.html_safe
   end
 
+
+  private
+  def link_text(object)
+    if object.file_content_type =~ /image/i
+      image_tag( object.file.url(:icon) ) +
+        "<br /><br />".html_safe +
+        object.file_file_name.truncate(80)
+    else
+      object.file_file_name.truncate(80)
+    end
+  end
 end

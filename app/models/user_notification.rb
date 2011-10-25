7e213371a4bd3f686efaeda540a12d9d7d8fbe2c
Andi Altendorfer <andi@iboard.cc>
2011-10-25 10:16:58 +0200

    Review
    Delivery of user_notifications

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -29,18 +29,29 @@ class UserNotification
   end
   
 private
+
+  # Copy the user_notification to all recipients but self (sender)
   def deliver_message
     return unless @recipients
-    self.message += "\n----\n" + I18n.translate(:delivered_to)
-    if( all_users = (@recipients.count == User.count))
-      self.message += I18n.translate(:all_users, count: @recipients.count)
-    end
-    @recipients.each do |usr|
-      usr.user_notifications.create(self.attributes).save unless usr == self.user
-      self.message += "\n *" + usr.name + " (#{usr.email})" unless all_users == true
+    append_delivery_notes
+    @recipients.reject{|r| r==self.user}.each do |usr|
+      usr.user_notifications.create(self.attributes).save 
       usr.save
     end
     self.save!
     self.user.save!
+  end
+  
+  # Append delivered to: All Users | list of users to the sender's copy of
+  # the user_notification. Will be called from deliver_message. Don't call otherwise
+  def append_delivery_notes
+    self.message += "\n----\n" + I18n.translate(:delivered_to)
+    if( all_users = (@recipients.count == User.count))
+      self.message += I18n.translate(:all_users, count: @recipients.count)
+    else
+      self.message += @recipients.map { |recipient|
+        recipient.name + " (#{recipient.email})"
+      }.join(", ")
+    end
   end
 end

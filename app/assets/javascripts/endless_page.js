0588a3e15a59f9a88877f631653b424d80c04e93
Andi Altendorfer <andi@iboard.cc>
2011-11-08 15:30:37 -0500

    Fix javascript while sorting site_menus

--- app/assets/javascripts/endless_page.js
+++ app/assets/javascripts/endless_page.js
@@ -8,34 +8,32 @@ function checkScroll() {
     $('#load_more_link').click();
   } else {
     setTimeout("checkScroll()", 500);
-  }  
+  }
 }
 
 function controllToTopLink() {
-  if ( $('#scroll-to-top')) {
-    if( onTop() ) {
-      if( state_scroll_to_top_link == 1 ) {
-        $('#scroll-to-top').effect('puff',{},500);
-        state_scroll_to_top_link = 0;
+  if( onTop() ) {
+    if( state_scroll_to_top_link == 1 ) {
+      $('#scroll-to-top').effect('puff',{},500);
+      state_scroll_to_top_link = 0;
+    }
+  } else {
+    if( state_scroll_to_top_link == 0 ) {
+      $('#scroll-to-top').fadeTo(250,0.75);
+      state_scroll_to_top_link = 1;
+    }
+
+    if(scrolledPercentage() >= 99) {
+      if( bottom_announced == 0){
+        $('#scroll-to-top').effect('pulsate',{times: 2},250);
+        bottom_announced = 1;
       }
     } else {
-      if( state_scroll_to_top_link == 0 ) {
-        $('#scroll-to-top').fadeTo(250,0.75);
-        state_scroll_to_top_link = 1;
-      }
-      
-      if(scrolledPercentage() >= 99) { 
-        if( bottom_announced == 0){
-          $('#scroll-to-top').effect('pulsate',{times: 2},250);
-          bottom_announced = 1;
-        }
-      } else {
-        bottom_announced = 0;
-      }
-      
+      bottom_announced = 0;
     }
-    setTimeout("controllToTopLink()", 500);
+
   }
+  setTimeout("controllToTopLink()", 500);
 }
 
 function nearBottomOfPage() {

1dde45b512871fe23cdd9604717fec4a64bbb0a1
Andi Altendorfer <andi@iboard.cc>
2011-11-09 14:54:48 -0500

    Fix home_controller_spec

--- spec/requests/home_controller_spec.rb
+++ spec/requests/home_controller_spec.rb
@@ -1,14 +1,14 @@
 require File::expand_path('../../spec_helper', __FILE__)
 
 describe "HomeController:" do
-  
+
   before(:all) do
     cleanup_database
     create_default_userset
     @news        = Blog.create(title: 'News', is_draft: false )
     @public_blog = Blog.create(title: 'Public Blog', is_draft: false)
     @private_blog= Blog.create(title: 'Private Blog', is_draft: false, user_role: 1)
-    
+
     @news.postings.create(
       title: 'A News Posting', is_draft: false, user: User.first,
       body: 'This is a public posting in blog News', tags: 'PubPost,NewsTag'
@@ -23,16 +23,16 @@ describe "HomeController:" do
     )
     visit root_path
   end
-      
-  describe "For unauthorized users" do    
+
+  describe "For unauthorized users" do
     it "should list public postings of blog News in root_path" do
       page.should have_content "A News Posting"
     end
     it "should not list private postings in root_path" do
       page.should have_no_content "A Private Posting"
     end
-    
-    describe "Blog index" do    
+
+    describe "Blog index" do
       it "should list public blogs in blogs index" do
         visit blogs_path
         page.should have_content "News"
@@ -51,11 +51,11 @@ describe "HomeController:" do
       end
       it "should not list private blog and postings" do
         visit blog_path(@private_blog)
-        page.should have_no_content "Private Blog"      
+        page.should have_no_content "Private Blog"
         page.should have_no_content "A Private Posting"
-      end      
+      end
     end
-  end 
+  end
 
 
   describe "For authorized users" do
@@ -63,7 +63,7 @@ describe "HomeController:" do
       log_in_as "user@iboard.cc", "thisisnotsecret"
       visit root_path
     end
-    
+
     describe "Blog index" do
       before(:each) do
         visit blogs_path
@@ -74,7 +74,7 @@ describe "HomeController:" do
         page.should have_content "Private Blog"
       end
     end
-    
+
     describe "Private Blog Show-view" do
       it "should list private postings" do
         visit blog_path(@private_blog)
@@ -82,8 +82,8 @@ describe "HomeController:" do
       end
     end
   end
-  
-  
+
+
   describe "TagCloud" do
     describe "For unauthorized users" do
       it "should show public posting through PubPost Tag" do
@@ -96,7 +96,7 @@ describe "HomeController:" do
         page.should have_no_link "PrivPost"
       end
       it "should not list private postings for tag PrivPost" do
-        visit tag_path('PrivPost')
+        visit tags_path('PrivPost')
         page.should have_no_content "A Private Posting"
       end
     end
